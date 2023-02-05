eb085d8d0709d986d5b7aeeac4949613c7fb5c1b
wansom <ochiengwarren10@gmail.com>
2023-02-05 10:58:03 +0300

    tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+const { fontFamily } = require("tailwindcss/defaultTheme");
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    container: {
+      screens: {
+        sm: "640px",
+        md: "768px",
+        lg: "1280px",
+        xl: "1440px",
+      },},
+    extend: {
+      fontFamily: {
+        sans: ["DM Sans", ...fontFamily.sans],
+        Montserrat: ["Montserrat", ...fontFamily.sans],
+      },
+    },
+  },
+  plugins: [
+    require("@tailwindcss/typography"),
+  ],
+}
