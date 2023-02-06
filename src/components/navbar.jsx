e6ff5e61b5aea8b2a11466a58bf4a0b3147ac593
wansom <ochiengwarren10@gmail.com>
2023-02-07 01:01:03 +0300

    smaller-navbar

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -6,7 +6,14 @@ const Navbar = () => {
       >
         <div className="w-full h-[170px] bg-[#FCFCFC]"></div>
       </div>
-      <div className="w-full h-[50px] primary-color">
+      <div className="w-full h-[50px] primary-color pl-[360px] flex gap-[50px] items-center">
+        <a className="nav-active" href="#">Home</a>
+        <a className="nav-link"  href="#">About Us</a>
+        <a className="nav-link"  href="#">Research</a>
+        <a className="nav-link"  href="#">Outreach Programs</a>
+        <a className="nav-link"  href="#">Think Tanks</a>
+        <a className="nav-link"  href="#">Publications</a>
+        <a className="nav-link"  href="#">Library</a>
       </div>
     </section>
   );
