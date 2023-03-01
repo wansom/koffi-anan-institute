d7a1f31bd997a0879de8f76167e64b9836baaa24
wansom <ochiengwarren10@gmail.com>
2023-03-01 09:55:52 +0300

    make research prjects dynamic

--- src/Data/home.js
+++ src/Data/home.js
@@ -24,3 +24,17 @@ export const services=[
     },
 
 ]
+
+export const researchProjects =[
+    {
+        title:" Hope for a better Future: Building Collaborative Resilience for Youth in Liberia",
+        subtitle:"  We also launched the Mary Antoinette Brown Sherman Think Tank, which will strengthen the position of girls and women in society through leadership and governance training programs, by providing a platform for discussion and through research.",
+        image_url:"images/home/Monument.png"
+    },
+    {
+        title:" Hope for a better Future: Building Collaborative Resilience for Youth in Liberia",
+        subtitle:"  We also launched the Mary Antoinette Brown Sherman Think Tank, which will strengthen the position of girls and women in society through leadership and governance training programs, by providing a platform for discussion and through research.",
+        image_url:"images/home/Monument.png"
+    }
+
+]
--- src/components/services.jsx
+++ src/components/services.jsx
@@ -1,7 +1,7 @@
-import { services } from "../Data/home";
+import { services, researchProjects } from "../Data/home";
 const Services = () => {
   return (
-    <section className="container  mx-auto relative bg-[#FCFCFC]  pt-[50px]  px-[30px]">
+    <section className="container  mx-auto relative bg-[#FCFCFC]  py-[50px]  px-[30px]">
       <div className="flex flex-col md:flex-row  justify-between">
         <div className="flex flex-col gap-[50px]">
           <p className="title text-center mt-[50px] md:mt-[100px] mb-[50px]">
@@ -12,54 +12,32 @@ const Services = () => {
             className="grid grid-cols-1 md:grid-cols-2 gap-[30px]
           "
           >
-            {services.map((service) => (
-              <div className="h-[300px] w-[335px] rounded-[5px] relative mb-[273px]" style={{backgroundImage: `url(${service.image_url})`}}>
+            {services.map((service, index) => (
+              <div
+                className="h-[300px] w-[335px] rounded-[5px] relative mb-[230px] lg:mb-[273px]"
+                style={{ backgroundImage: `url(${service.image_url})` }}
+              >
                 <div className="service-card absolute top-[235px] left-[20px] w-[295px] h-[263px] space-y-[21px] ">
-                  <p className="text-center card-title">01</p>
+                  <p className="text-center card-title">{index}</p>
                   <p className="text-center card-title">{service.title}</p>
-                  <p className="subtitle">
-                   {service.subtitle}
-                  </p>
+                  <p className="subtitle">{service.subtitle}</p>
                 </div>
               </div>
             ))}
           </div>
-
-          <div className=" w-[800px] h-[1158px] pt-[100px] px-[50px]">
+          <div className="pt-[50px]">
             <p className="title text-center mb-[50px]">Research Projects</p>
-            <div className="grid grid-cols-2">
-              <div className="w-[335px] h-[838px] flex flex-col gap-[20px] border-[#E0E2F1] border-solid border-[1px] rounded-[5px]">
-                <img src="images/home/Monument.png" alt="" />
-                <p className="blog-title mx-[20px]">
-                  Hope for a better Future: Building Collaborative Resilience
-                  for Youth in Liberia
-                </p>
-                <p className="blog-subtitle mx-[20px]">
-                  We also launched the Mary Antoinette Brown Sherman Think Tank,
-                  which will strengthen the position of girls and women in
-                  society through leadership and governance training programs,
-                  by providing a platform for discussion and through research.
-                </p>
-                <button className="research-btn text-[#FCFCFC] mx-[20px]">
-                  Learn More
-                </button>
-              </div>
-              <div className="w-[335px] h-[838px] flex flex-col gap-[20px] border-[#E0E2F1] border-solid border-[1px] rounded-[5px]">
-                <img src="images/home/Monument.png" alt="" />
-                <p className="blog-title mx-[20px]">
-                  Hope for a better Future: Building Collaborative Resilience
-                  for Youth in Liberia
-                </p>
-                <p className="blog-subtitle mx-[20px]">
-                  We also launched the Mary Antoinette Brown Sherman Think Tank,
-                  which will strengthen the position of girls and women in
-                  society through leadership and governance training programs,
-                  by providing a platform for discussion and through research.
-                </p>
-                <button className="research-btn text-[#FCFCFC] mx-[20px]">
-                  Learn More
-                </button>
-              </div>
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-[30px]">
+              {researchProjects.map((project) => (
+                <div className="w-[335px] h-[838px] flex flex-col gap-[20px] border-[#E0E2F1] border-solid border-[1px] rounded-[5px]">
+                  <img src="images/home/Monument.png" alt="" />
+                  <p className="blog-title mx-[20px]">{project.title}</p>
+                  <p className="blog-subtitle mx-[20px]">{project.subtitle}</p>
+                  <button className="research-btn text-[#FCFCFC] mx-[20px]">
+                    Learn More
+                  </button>
+                </div>
+              ))}
             </div>
           </div>
         </div>
