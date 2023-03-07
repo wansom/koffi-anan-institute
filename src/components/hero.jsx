b2e948975752101c74dc128ca09b4362b4730db0
wansom <ochiengwarren10@gmail.com>
2023-03-07 14:44:58 +0300

    add classname to hero section

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -1,9 +1,9 @@
 const Hero = () => {
   return (
-    <section className="banner">
-    <div className="banner-mask">
-        <div className="banner-container container">
-            <div className="banner-text">
+    <section class="banner">
+    <div class="banner-mask">
+        <div class="banner-container container">
+            <div class="banner-text">
                 <h1>Welcome to the Kofi Annan Institute for Conflict Transformation</h1>
                 <p> The Kofi Annan Institute for Conflict Transformation celebrated International Women's Day
                     with the KAICT family, members of the University of Liberia, representatives of government,
