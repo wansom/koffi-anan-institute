2369ef6d944219a031f454e42b0a294993a18cd4
wansom <ochiengwarren10@gmail.com>
2023-03-07 10:46:24 +0300

    update:thinks-tanks

--- src/components/think-tanks.jsx
+++ src/components/think-tanks.jsx
@@ -1,15 +1,15 @@
 const ThinkTanks = () => {
   return (
-    <section className="think-tanks">
-    <div className="think-tanks-container container">
-        <div className="think-head">
+    <section class="think-tanks">
+    <div class="think-tanks-container container">
+        <div class="think-head">
             <h1>Think Tanks</h1>
         </div>
-        <div className="think-tank-content">
-            <div className="think-tank-card">
+        <div class="think-tank-content">
+            <div class="think-tank-card">
                 <img src="images/home/think-tanks.svg"
                     alt="Kofi Annan Institute for Conflict Transformation Think Tanks"></img>
-                <div className="think-tank-info">
+                <div class="think-tank-info">
                     <h2>Mary Antoinette Brown Sherman Think Tank</h2>
                     <p>We also launched the Mary Antoinette Brown Sherman Think Tank, which will strengthen the
                         position of girls and women in society through leadership and governance training
@@ -17,8 +17,8 @@ const ThinkTanks = () => {
                     <button>Learn More</button>
                 </div>
             </div>
-            <div className="think-tank-card">
-                <div className="think-tank-info">
+            <div class="think-tank-card">
+                <div class="think-tank-info">
                     <h2>Mary Antoinette Brown Sherman Think Tank</h2>
                     <p>We also launched the Mary Antoinette Brown Sherman Think Tank, which will strengthen the
                         position of girls and women in society through leadership and governance training
@@ -26,37 +26,7 @@ const ThinkTanks = () => {
                     <button>Learn More</button>
                 </div>
                 <img src="images/home/think-tanks.svg"
-                    alt="Kofi Annan Institute for Conflict Transformation Think Tanks"></img>  <section className="think-tanks">
-                    <div className="think-tanks-container container">
-                        <div className="think-head">
-                            <h1>Think Tanks</h1>
-                        </div>
-                        <div className="think-tank-content">
-                            <div className="think-tank-card">
-                                <img src="images/home/think-tanks.svg"
-                                    alt="Kofi Annan Institute for Conflict Transformation Think Tanks"></img>
-                                <div className="think-tank-info">
-                                    <h2>Mary Antoinette Brown Sherman Think Tank</h2>
-                                    <p>We also launched the Mary Antoinette Brown Sherman Think Tank, which will strengthen the
-                                        position of girls and women in society through leadership and governance training
-                                        programs, by providing a platform for discussion and through research.</p>
-                                    <button>Learn More</button>
-                                </div>
-                            </div>
-                            <div className="think-tank-card">
-                                <div className="think-tank-info">
-                                    <h2>Mary Antoinette Brown Sherman Think Tank</h2>
-                                    <p>We also launched the Mary Antoinette Brown Sherman Think Tank, which will strengthen the
-                                        position of girls and women in society through leadership and governance training
-                                        programs, by providing a platform for discussion and through research.</p>
-                                    <button>Learn More</button>
-                                </div>
-                                <img src="images/home/think-tanks.svg"
-                                    alt="Kofi Annan Institute for Conflict Transformation Think Tanks"></img>
-                            </div>
-                        </div>
-                    </div>
-                </section>
+                    alt="Kofi Annan Institute for Conflict Transformation Think Tanks"></img>
             </div>
         </div>
     </div>
