962a4e517e8845044265a8df5c404efe81189aa1
wansom <ochiengwarren10@gmail.com>
2023-03-22 09:34:55 +0300

    remove single news

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import AboutRoute from "./pages/about";
 import ContactUsRoute from "./pages/contact-us";
 import AllNews from "./pages/all-news";
-import SingleNews from "./pages/single-news";
+// import SingleNews from "./pages/single-news";
 import AllEvents from "./pages/all-events";
 import SingleEvent from "./pages/single-event";
 import SingleService from "./pages/single-service";
@@ -23,7 +23,7 @@ root.render(
       <Route path="/about-us" element={<AboutRoute/>}/>
       <Route path="/contact-us" element={<ContactUsRoute/>}/>
       <Route path="/all-news" element={<AllNews/>}/>
-      <Route path="/articles" element={<SingleNews/>}/>
+      {/* <Route path="/articles" element={<SingleNews/>}/> */}
       <Route path="/all-events" element={<AllEvents/>}/>
       <Route path="/event" element={<SingleEvent/>}/>
       <Route path="/teaching-and-training" element={<SingleService/>}/>
