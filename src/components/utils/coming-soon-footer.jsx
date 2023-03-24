7dbe91447334709c7bcf9ed2ce0d42f315a84d8a
wansom <ochiengwarren10@gmail.com>
2023-03-24 12:25:27 +0300

    coming soon complete

---
+++ src/components/utils/coming-soon-footer.jsx
@@ -0,0 +1,18 @@
+const ComingSoonFooter=()=>{
+    return(
+<div className="coming-soon-footer absolute bottom-0 left-0 w-full py-6">
+<div className="container flex justify-between items-center ">
+    <p>Kofi Annan Institute for Conflict Transformation. Rights Reserved. Copyright 2022.</p>
+    <div className="flex gap-4">
+        <a href=""> <i className="fa fa-facebook text-24 text-white"></i></a>
+        <a href=""> <i className="fa fa-twitter text-24 text-white"></i></a>
+        <a href=""> <i className="fa fa-linkedin text-24 text-white"></i></a>
+
+
+    </div>
+    
+</div>
+</div>
+    )
+}
+export default ComingSoonFooter
