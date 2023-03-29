91eb161b64e7222c835b49422b8e932788d8b1ec
wansom <ochiengwarren10@gmail.com>
2023-03-29 17:11:28 +0300

    dheh

--- src/components/research-projects/all-research.jsx
+++ src/components/research-projects/all-research.jsx
@@ -1,18 +1,29 @@
-const AllResearch=()=>{
-    return (
-        <section className="container mx-auto pt-10 all-research">
-            <div className="space-y-8 mt-10">
-                <h1>Ongoing Projects</h1>
-                <hr />
-                <div className="relative card bg-center bg-cover" style={{backgroundImage:'url(../images/outreach-program/r-1.png)'}}>
-                    <div className="absolute left-0 bottom-0 card-inner">
-                        <h3>The Role of Women in Conflict Prevention and Peacebuilding in Liberia</h3>
-                        <p>The project seeks to explore the role of women in conflict prevention and peacebuilding in Liberia, with a focus on their contributions to building sustainable peace and preventing future conflicts.</p>
-                    </div>
-                </div>
-
-            </div>
-        </section>
-    )
-}
-export default AllResearch
+const AllResearch = () => {
+  return (
+    <section className="container mx-auto pt-10 all-research">
+      <div className="space-y-8 mt-10">
+        <h1>Ongoing Projects</h1>
+        <hr />
+        <div
+          className="relative card bg-center bg-cover"
+          style={{ backgroundImage: "url(../images/outreach-program/r-1.png)" }}
+        >
+          <div className="absolute left-0 bottom-0 card-inner space-y-2">
+            <h3>
+              The Role of Women in Conflict Prevention and Peacebuilding in
+              Liberia
+            </h3>
+            <p>
+              The project seeks to explore the role of women in conflict
+              prevention and peacebuilding in Liberia, with a focus on their
+              contributions to building sustainable peace and preventing future
+              conflicts.
+            </p>
+            <span>read more</span>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+export default AllResearch;
