da25b875b061fbb1787e38aaa6d497157e058fe4
wansom <ochiengwarren10@gmail.com>
2023-05-27 12:53:24 +0300

    update:why us

--- src/components/about/choices.jsx
+++ src/components/about/choices.jsx
@@ -1,46 +1,72 @@
-const Choices=()=>{
+const Choices = () => {
   return (
     <section class="choices">
-    <div class="choices-mask">
+      <div class="choices-mask">
         <div class="choices-container container">
-            <div class="choice-head">
-                <p>Why Choose Us</p>
-                <h1>Areas of Engagement</h1>
+          <div class="choice-head">
+            <p>Why Choose Us</p>
+            <h1>Areas of Engagement</h1>
+          </div>
+          <div class="choice-content">
+            <div class="row">
+              <div class="column choice">
+                <h3>Education & Training</h3>
+                <p>
+                  The Institute's primary focus is on providing high-quality
+                  education and training in conflict resolution and
+                  peacebuilding. It offers academic programs, training courses,
+                  and professional development opportunities that equip
+                  individuals with the knowledge, skills, and values needed to
+                  address conflicts effectively.
+                </p>
+              </div>
+              <div class="column choice">
+                <h3>Research</h3>
+                <p>
+                  The Institute conducts research and scholarship in the field
+                  of conflict resolution and peacebuilding, with a focus on
+                  practical applications and real-world impact. Its research
+                  informs its teaching and training activities and contributes
+                  to the broader academic and policy discourse on peace and
+                  conflict issues.
+                </p>
+              </div>
+              <div class="column choice">
+                <h3>Policy Engagement</h3>
+                <p>
+                  The Institute advocates for policies and practices that
+                  promote conflict resolution and peacebuilding. It engages with
+                  policymakers and stakeholders to promote evidence-based
+                  approaches to conflict resolution and to inform policy
+                  decisions.
+                </p>
+              </div>
+              <div class="column choice">
+                <h3>Partnerships and Collaboration</h3>
+                <p>
+                  The Institute values partnerships and collaboration with
+                  governments, civil society organizations, academic
+                  institutions, and individuals. It works closely with these
+                  stakeholders to advance its mission and to promote conflict
+                  resolution and peacebuilding more broadly.
+                </p>
+              </div>
+              <div class="column choice">
+                <h3>Outreach Programs</h3>
+                <p>
+                  The Institute is committed to engaging with communities
+                  affected by conflicts and promoting dialogue and
+                  reconciliation. It works closely with local partners to
+                  develop and implement programs that address specific conflict
+                  issues and build sustainable peace.
+                </p>
+              </div>
+              <div class="column"></div>
             </div>
-            <div class="choice-content">
-                <div class="row">
-                    <div class="column choice">
-                        <h3>Education & Training</h3>
-                        <p>The Institute's primary focus
-                            is on providing high-quality education and training in conflict resolution and peacebuilding. It offers academic programs, training courses, and professional development opportunities that equip individuals with the knowledge, skills, and values needed to address conflicts effectively.</p>
-                    </div>
-                    <div class="column choice">
-                        <h3>Education & Training</h3>
-                        <p>The Institute's primary focus
-                            is on providing high-quality education and training in conflict resolution and peacebuilding. It offers academic programs, training courses, and professional development opportunities that equip individuals with the knowledge, skills, and values needed to address conflicts effectively.</p>
-                    </div>
-                    <div class="column choice">
-                        <h3>Education & Training</h3>
-                        <p>The Institute's primary focus
-                            is on providing high-quality education and training in conflict resolution and peacebuilding. It offers academic programs, training courses, and professional development opportunities that equip individuals with the knowledge, skills, and values needed to address conflicts effectively.</p>
-                    </div>
-                    <div class="column choice">
-                        <h3>Education & Training</h3>
-                        <p>The Institute's primary focus
-                            is on providing high-quality education and training in conflict resolution and peacebuilding. It offers academic programs, training courses, and professional development opportunities that equip individuals with the knowledge, skills, and values needed to address conflicts effectively.</p>
-                    </div>
-                    <div class="column choice">
-                        <h3>Education & Training</h3>
-                        <p>The Institute's primary focus
-                            is on providing high-quality education and training in conflict resolution and peacebuilding. It offers academic programs, training courses, and professional development opportunities that equip individuals with the knowledge, skills, and values needed to address conflicts effectively.</p>
-                    </div>
-                    <div class="column">
-                    </div>
-                </div>
-            </div>
+          </div>
         </div>
-    </div>
-</section>
-  )
-}
- export default Choices
+      </div>
+    </section>
+  );
+};
+export default Choices;
