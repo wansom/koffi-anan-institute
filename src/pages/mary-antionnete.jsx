30d5f75eccbaf4cc2da8e99f3fb9eb8e8adf3922
Clinton-dev <clintonwambugu@gmail.com>
2023-07-05 13:57:04 +0300

    Change order of research projects in homepage

--- src/pages/mary-antionnete.jsx
+++ src/pages/mary-antionnete.jsx
@@ -52,36 +52,14 @@ const MaryAntoinnette = () => {
             <div>
                 <h2 className="think-head">Public Lectures</h2>
                 <p className="think-p">
-                  Our public lectures cover a wide range of security-related topics,
-                  including security sector reform, peacebuilding, conflict
-                  prevention, and post-conflict reconstruction. We invite experts,
-                  policymakers, and practitioners from academia, government, civil
-                  society, and the private sector to share their insights and
-                  experiences on these topics. Our lectures provide a platform for
-                  robust discussions and debates, enabling participants to exchange
-                  ideas and learn from one another.
+                  Recognizing the need to discuss the role of girls and women in
+                  society and acknowledging their contribution to public life,
+                  the Mary Antoinette Brown Sherman Think Tank launched its
+                  lecture series on November 24, 2022. The series features notable
+                  women who have contributed to public life in Liberia, women’s champions,
+                  and discussions around gender, women and girls.
                 </p>
-                <p className="think-p">
-                  Our public lectures are open to the public and are held at various
-                  locations, including the Kofi Annan Institute for Conflict
-                  Transformation, University of Liberia, and other partner
-                  institutions. We also organize virtual lectures, making them
-                  accessible to a wider audience across Liberia and beyond. Our
-                  lectures are an excellent opportunity for students, researchers,
-                  policymakers, practitioners, and the general public to engage with
-                  leading experts in the field of security and learn about the
-                  latest developments, challenges, and best practices.
-                </p>
-                <p className="think-p">
-                  In addition to our public lectures, we also organize workshops,
-                  seminars, and roundtable discussions on security-related topics.
-                  These events provide a more focused and interactive platform for
-                  participants to engage with the speakers and discuss specific
-                  issues in more detail. Our workshops and seminars are designed for
-                  policymakers, practitioners, and researchers who want to deepen
-                  their knowledge and understanding of security-related issues and
-                  explore potential solutions to address them.
-                </p>
+               
                 <div className="lecture-cards">
                   <div className="lecture-card">
                     <img src="/images/video.png" alt="Lecuture Thumbnail KAICT" />
@@ -153,50 +131,23 @@ const MaryAntoinnette = () => {
             <div>
                 <h2 className="think-head">Training</h2>
                 <p className="think-p">
-                  Our training programs are led by experienced SSR practitioners,
-                  academics, and policymakers, who have a wealth of expertise in the
-                  field. They provide participants with the opportunity to learn
-                  from and engage with experts from around the world, while also
-                  benefiting from the Institute's unique location in Liberia.
+                Recognizing the need to build girls’ and women’s self-confidence
+                and skills for their full participation in public life, the Mary
+                Antoinette Brown Sherman Think Tank offers a variety of skills
+                training programs for girls and women focused on public speaking,
+                leadership and governance, amongst others. These include both
+                in-house training and outreach programs in communities across Liberia.
                 </p>
-                <p className="title-list">
-                  Our training programs cover a range of topics related to SSR,
-                  including:
-                </p>
-                <ul className="think-list">
-                  <li>
-                    Understanding the role of security forces in democratic
-                    governance
-                  </li>
-                  <li>Developing and implementing effective SSR strategies</li>
-                  <li>The role of civil society in SSR</li>
-                  <li>Gender and SSR</li>
-                  <li>Human rights and SSR</li>
-                  <li>The role of international actors in SSR</li>
-                </ul>
-                <p className="think-p">
-                  Our training programs are designed to be flexible and adaptable,
-                  and can be tailored to meet the specific needs of individual
-                  organizations and groups. They can be delivered in-person or
-                  online, and can be structured as one-day workshops, multi-day
-                  training sessions, or longer-term courses.
-                </p>
-                <p className="think-p">
-                  Whether you are a practitioner seeking to enhance your skills in
-                  the field of SSR, or an organization looking to build your
-                  capacity in this area, the SSR Think Tank at the Kofi Annan
-                  Institute for Conflict Transformation is here to help. Contact us
-                  today to learn more about our training programs and how we can
-                  support your professional development in the field of SSR.
-                </p>
+
             </div>
 
             {/* Research section */}
             <div>
                 <h2 className="think-head">Research</h2>
                 <p className="think-p">
-                  Our research focuses on a wide range of security sector reform
-                  topics, including but not limited to:
+                Recognizing the need for evidence-based learning, teaching and
+                participation in public life, the Think Tank engages in research
+                on gender in Liberia.
                 </p>
                 <div className="takeaway-cards">
                   <div className="takeaway-card">
@@ -222,43 +173,12 @@ const MaryAntoinnette = () => {
                   </div>
                   <div className="takeaway-card">
                     <div className="card-center">
-                      <h2>Gender & Security</h2>
+                      <h2>Training</h2>
                       <p>
-                        The study of how gender shapes experiences of insecurity and
-                        the ways in which gender roles and power dynamics influence
-                        security policies and practices.
-                      </p>
-                    </div>
-                  </div>
-                  <div className="takeaway-card">
-                    <div className="card-center">
-                      <h2>Border Security</h2>
-                      <p>
-                        The measures taken by governments to secure their borders,
-                        including the management of migration flows, the prevention
-                        of cross-border crime, and the protection of national
-                        sovereignty.
-                      </p>
-                    </div>
-                  </div>
-                  <div className="takeaway-card">
-                    <div className="card-center">
-                      <h2>Disarmament, Demobilization & Reintegration</h2>
-                      <p>
-                        The process of disarming, demobilizing and reintegrating
-                        former combatants into civilian life, crucial for
-                        post-conflict stabilization and peacebuilding.
-                      </p>
-                    </div>
-                  </div>
-                  <div className="takeaway-card">
-                    <div className="card-center">
-                      <h2>Justice & Security</h2>
-                      <p>
-                        The intersection between security and justice sectors,
-                        including the role of the judiciary, legal frameworks, and
-                        human rights in ensuring effective and accountable security
-                        provision.
+                        The SSR Think Tank also offers training courses, in particular
+                        for security sector personnel and civil society activists.
+                        The trainings focus on strengthening democratic attitudes,
+                        respect for human rights, and sensitivity to gender disparities.
                       </p>
                     </div>
                   </div>
