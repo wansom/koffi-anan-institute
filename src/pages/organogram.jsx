2577836b2deb39df2292cfa8b203d689b8508f30
Clinton-dev <clintonwambugu@gmail.com>
2023-07-05 10:29:45 +0300

    Correct organogram heading color

--- src/pages/organogram.jsx
+++ src/pages/organogram.jsx
@@ -5,14 +5,16 @@ import backgound from "../hero/about.png";
 
 const Organogram=()=>{
     return (
-        <div className="team-container bg-[#EFF1FB]">
+        <div className="team-container ">
             <Navbar/>
             <AboutHero
         subtitle={"Know Us Better"}
         title={"About Us"}
         background={backgound}
       />
-      <div className=" container mx-auto bg-[#EFF1FB]">
+      <div className="bg-[#EFF1FB]">
+
+      <div className="container mx-auto ">
         <div className="flex items-center flex-col justify-center psace-y-10 mt-10">
           <div className="card py-6">
             <h5>Director</h5>
@@ -50,17 +52,17 @@ const Organogram=()=>{
           <div className="card py-4">
             <h5>Deputy Director for Policy Dialogue & Special Projects</h5>
             <p>Dr. Anthony L. Hena</p>
-          </div> 
+          </div>
           <hr className="vertical-hr" />
           <div className="card py-6">
             <h5>Deputy Director for Research</h5>
             <p>Dr. Raphaela T. Kormoll</p>
-          </div> 
+          </div>
           <hr className="vertical-hr" />
           <div className="card py-6">
             <h5>Deputy Director for Teaching</h5>
             <p>Vacant</p>
-          </div> 
+          </div>
           </div>
           <hr className="horizontal-hr mt-12" />
           </div>
@@ -69,39 +71,40 @@ const Organogram=()=>{
           <div className="card py-4">
             <h5>Deputy Director for Research</h5>
             <p>Dr. Raphaela T. Kormoll</p>
-          </div> 
+          </div>
           <hr className="vertical-hr" />
           <div className="card py-6">
             <h5>Research Faculty</h5>
             <p>Permanent & Visiting</p>
-          </div> 
+          </div>
           <hr className="vertical-hr" />
           <div className="card py-6">
             <h5>Research Assistants</h5>
             <p>Temporary</p>
-          </div> 
+          </div>
           </div>
           <hr className="horizontal-hr mt-12" />
           </div>
-        
+
 
           <div className="flex flex-col items-center justify-center">
           <div className="card py-4">
             <h5>Deputy Director for Teaching</h5>
             <p>Vacant</p>
-          </div> 
+          </div>
           <hr className="vertical-hr" />
           <div className="card py-6">
             <h5>Part- Time Faculty</h5>
             <p>Vacant</p>
-          </div> 
+          </div>
           <hr className="vertical-hr" />
           <div className="card py-6">
             <h5>Librarian</h5>
             <p>Vacant</p>
-          </div> 
+          </div>
           </div>
         </div>
+      </div>
       </div>
       <Footer/>
 
