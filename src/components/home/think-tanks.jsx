7f11a1b47ab4fd2c5d74cf32349ef52cc9890fc8
wansom <ochiengwarren10@gmail.com>
2023-08-04 13:14:55 +0300

    update the content

--- src/components/home/think-tanks.jsx
+++ src/components/home/think-tanks.jsx
@@ -1,10 +1,10 @@
 import { useState, useEffect } from "react"
 import { getData } from '../../services';
 const ThinkTanks = () => {
-    const [posts,setPosts]= useState([]);
-    const [loading, setloading] = useState(true);
-    useEffect(() => {
-      fetch("https://kacit.twafwane.com/wp-json/wp/v2/thinkthanks")
+  const [posts, setPosts] = useState([]);
+  const [loading, setloading] = useState(true);
+  useEffect(() => {
+    fetch("https://kacit.twafwane.com/wp-json/wp/v2/thinkthanks")
       .then((response) => response.json())
       .then((posts) => {
         const promises = posts.map((post) => {
@@ -24,28 +24,41 @@ const ThinkTanks = () => {
         setloading(false);
       })
       .catch((error) => console.error(error));
-    }, []);
+  }, []);
 
   return (
     <section className="think-tanks">
-    <div className="think-tanks-container container">
+      <div className="think-tanks-container container">
         <div className="think-head">
-            <h1>Policy Engagement</h1>
+          <h1>Policy Engagement</h1>
         </div>
         <div className="think-tank-content">
-            {posts.map((post,index)=>(
-            <div className="think-tank-card grid gird-cols-1 md:grid-cols-2 gap-10 mb-6" key={index}>
-            <img src={post.featured_image_url} alt="Kofi Annan Institute for Conflict Transformation Think Tanks" className="h-full object-contain md:h-[80%] w-full"></img>
-            <div className="think-tank-info">
-                <h2  dangerouslySetInnerHTML={{ __html: post.acf.title }}></h2>
-                <p dangerouslySetInnerHTML={{ __html: post.acf.overview }}></p>
-               <a href={post.acf.link}> <button>Learn More</button></a>
+          {posts.map((post, index) => (
+            <div  key={index}>
+              {index % 2 === 0 ?
+                <div className="grid gird-cols-1 md:grid-cols-2 gap-10 mb-6">
+                  <img src={post.featured_image_url} alt="Kofi Annan Institute for Conflict Transformation Think Tanks" className="h-full object-cover md:h-[320px] w-full"></img>
+                  <div className="think-tank-info">
+                    <h2 dangerouslySetInnerHTML={{ __html: post.acf.title }}></h2>
+                    <p dangerouslySetInnerHTML={{ __html: post.acf.overview }}></p>
+                    <a href={post.acf.link}> <button>Learn More</button></a>
+                  </div>
+                </div> :
+                <div className="grid gird-cols-1 md:grid-cols-2 gap-10 mb-6">
+                  <div className="think-tank-info">
+                    <h2 dangerouslySetInnerHTML={{ __html: post.acf.title }}></h2>
+                    <p dangerouslySetInnerHTML={{ __html: post.acf.overview }}></p>
+                    <a href={post.acf.link}> <button>Learn More</button></a>
+                  </div>
+                  <img src={post.featured_image_url} alt="Kofi Annan Institute for Conflict Transformation Think Tanks" className="h-full object-cover md:h-[320px] w-full"></img>
+
+                </div>}
+
             </div>
+          ))}
         </div>
-            ))}
-        </div>
-    </div>
-</section>
+      </div>
+    </section>
   );
 };
 
